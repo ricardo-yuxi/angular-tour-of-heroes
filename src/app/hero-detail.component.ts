336f4c9e64f7566ae7369963ed254cd25c6f01d9
Ricardo Rojas <reddrum7@gmail.com>
2017-07-20 21:34:48 -0500

    Finish Multiple Components chapter

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -2,7 +2,8 @@ import { Hero } from './hero';
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'hero-detail'
+  selector: 'hero-detail',
+  templateUrl: './hero-detail.component.html'
 })
 export class HeroDetailComponent {
   @Input() hero: Hero;
