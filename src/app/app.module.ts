e08826ff1da046f54437ef738aca888b6e976253
Ricardo Rojas <reddrum7@gmail.com>
2017-07-21 22:56:42 -0500

    Finish HTTP chapter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from "app/app-routing.module";
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from "app/heroes.component";
 import { HeroService } from "app/hero.service";
 import { DashboardComponent } from "app/dashboard.component";
-import { AppRoutingModule } from "app/app-routing.module";
+import { HeroSearchComponent } from './hero.search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   providers: [HeroService],
